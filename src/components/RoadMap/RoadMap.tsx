62578afbd9a6e99c9a4c616ef773e80558cb29c7
yandoshchinskiy <yan.doshchinskiy@gmail.com>
2022-02-19 07:47:48 +0700

    roadmap border fixed

--- src/components/RoadMap/RoadMap.tsx
+++ src/components/RoadMap/RoadMap.tsx
@@ -5,18 +5,18 @@ import { ReactComponent as Collage } from "assets/img/roadmap/collage.svg";
 export const RoadMap = () => {
     return (
         <div className="roadmap" id="roadmap">
-            <Collage className="roadmap__image" />
+            <Collage className="roadmap__image" /> {/* position absolute */}
             <div />
             <div className="roadmap__right">
-                {/* <h3 className="roadmap__title">ROADMAP</h3> */}
-                <div className="roadmap__content">
-                    <div className="roadmap__list">
-                        <div className="roadmap__text">He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s</div>
-                        <div className="roadmap__text">He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s</div>
-                        <div className="roadmap__text">He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s</div>
-                        <div className="roadmap__text">He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s</div>
-                        <div className="roadmap__text">He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s</div>
-                    </div>
+                <div className="roadmap__list">
+                    <div className="roadmap__text">He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s</div>
+                    <div className="roadmap__text">He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s</div>
+                    <div className="roadmap__text">He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s</div>
+                    <div className="roadmap__text">He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s</div>
+                    <div className="roadmap__text">He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s</div>
+                    <div className="roadmap__text">He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s</div>
+                    <div className="roadmap__text">He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s</div>
+                    <div className="roadmap__text">He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s He standard Lorem Ipsum passage, used since the 1500s</div>
                 </div>
             </div>
         </div>
