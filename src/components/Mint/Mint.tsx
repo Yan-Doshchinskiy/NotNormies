e1f03fe13b48602fb71e29c463049a9437addbf6
Yan.Doshinskiy <yan.doshinskiy@softlex.pro>
2022-03-17 09:59:18 +0700

    feat: mint adaptiv completed

--- src/components/Mint/Mint.tsx
+++ src/components/Mint/Mint.tsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import './Mint.scss';
+
 import { ReactComponent as Minter } from "assets/img/mint/minter.svg";
 import BaseButton from "components/ui/BaseButton/BaseButton";
 import BaseBadge from "components/ui/BaseBadge/BaseBadge";
 import { getMaxMintAmount, getMaxSupply, getTotalSupply, getUserBalance } from "../../utils/web3/view";
 import { getWalletAddress } from "../../utils/web3/connection";
 import mintToken from "../../utils/web3/mint";
+
 
 
 interface IMintProps {
@@ -70,36 +72,44 @@ export const Mint = ({ isConnected, handleConnect }: IMintProps) => {
     // template
     return (
         <div className="mint">
-            <div className="mint__anchor" id="mint" />
-            <div />
-            <h3 className="mint__title">Zdorova dolboebi</h3>
-            <Minter className="mint__img" />
-            <div className="mint__description">
-                <p className="mint__text">
-                    He standard Lorem Ipsum passage, used since the 1500s
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum.
-                    Section 1.10.32 of de Finibus Bonorum et Malorum, written by Cicero in 45 BC
-                </p>
-                <div className="mint__panel">
-                    {!isLoading
-                        && <BaseBadge
-                            className="mint__counter"
-                        >
-                            {totalSupply} / {supplyLimit}
-                        </BaseBadge>
-                    }
-                    <BaseButton
-                        className="mint__button"
-                        onClick={() => isConnected ? handleMint() : handleConnect()}
-                        disabled={!isMintAvailable || isLoading}
+            <div className="mint__anchor" id="mint"/>
+            <div className="mint__hidden" />
+            <h3 className="mint__title">Hi There</h3>
+            <div className="mint__left">
+                <Minter className="mint__img"/>
+            </div>
+            <p className="mint__text">
+                He standard Lorem Ipsum passage, used since the 1500s
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                culpa qui officia deserunt mollit anim id est laborum.
+                Section 1.10.32 of de Finibus Bonorum et Malorum, written by Cicero in 45 BC
+                He standard Lorem Ipsum passage, used since the 1500s
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                culpa qui officia deserunt mollit anim id est laborum.
+                Section 1.10.32 of de Finibus Bonorum et Malorum, written by Cicero in 45 BC
+            </p>
+            <div className="mint__hidden" />
+            <div className="mint__panel">
+                {!isLoading
+                    && <BaseBadge
+                        className="mint__counter"
                     >
-                        {isConnected ? getButtonText() : "Connect"}
-                    </BaseButton>
-                </div>
+                        {totalSupply} / {supplyLimit}
+                    </BaseBadge>
+                }
+                <BaseButton
+                    className="mint__button"
+                    onClick={() => isConnected ? handleMint() : handleConnect()}
+                    disabled={!isMintAvailable || isLoading}
+                >
+                    {isConnected ? getButtonText() : "Connect"}
+                </BaseButton>
             </div>
         </div>
     );
